b9079d02196280a03d65add9e179b549b67d207d
bernadette <aguirre.bernadette@outlook.com>
2022-05-25 16:07:38 -0600

    Completed Assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -5,7 +5,10 @@ import React from "react";
 import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+  render(<Header/ > );
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+
+let isHead = document.querySelector("head");
+isHead.textContent = "hello from the Header!";
